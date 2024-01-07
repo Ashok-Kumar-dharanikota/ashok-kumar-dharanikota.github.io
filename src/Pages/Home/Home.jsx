a7d2d2e7a758f1c55398bace50e4265723de929b
Ashok-Kumar-dharanikota <ashok81432@gmail.com>
2024-01-07 23:46:59 +0530

    Working on home page

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -24,6 +24,10 @@ function Home() {
 
       <SocialCard />
 
+      {/* <iframe style="border-radius:12px" src="https://open.spotify.com/embed/track/3ngc7rGlfeFaskU90EWl4R?utm_source=generator" width="40%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe> */}
+
+      <iframe style={{borderRadius: 12, gridArea: "box"}} src='https://open.spotify.com/embed/track/3ngc7rGlfeFaskU90EWl4R?utm_source=generator' width={"100%"} height={"100%"} frameBorder={0} allowFullScreen allow='autoplay' loading='lazy'> </iframe>
+
 
     </div>
   )
